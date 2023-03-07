1302a4a84dd4eac2b887fc8dd7d6ccd254fd44eb
Ecelon9 <Ecelon93@gmail.com>
2023-03-07 13:59:34 +0100

    finito esercizio array Java

--- Java/lezione5Java/src/EsArray.java
+++ Java/lezione5Java/src/EsArray.java
@@ -1,6 +1,18 @@
-public class EsArray {
+import util.Util;
+import java.util.Scanner;
 
+public class EsArray {
     public static void main(String[] args) {
+        int[] arrayNumeri1 = {5, 2, 4, 2, 1, 2};
+        int[] arrayNumeri2 = {2, 1, 2, 9, 3, 5};
+
+        System.out.println("Il numero minimo è: " + Util.numMinimo(arrayNumeri1));
+        System.out.println("L'indice di numero minimo è: " + Util.indiceMinimo(arrayNumeri1));
+        Util.sommaVettoriale(arrayNumeri1, arrayNumeri2);
+
+        System.out.println();
+        int[] a = Util.crea(3);
 
     }
+
 }
--- Java/lezione5Java/src/util/Util.java
+++ Java/lezione5Java/src/util/Util.java
@@ -7,16 +7,20 @@ public class Util {
     public static void riempi(int[] array) {
         Scanner input = new Scanner(System.in);
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Inserisci un numero: ");
+            System.out.println("Riempi un array: inserisci un numero: ");
             array[i] = Integer.parseInt(input.nextLine());
 
         }
+        input.close();
+
     }
 
     public static int[] crea(int n) {
         int[] array = new int[n];
         riempi(array);
+        stampa(array);
         return array;
+
     }
 
     //creare il metodo stampa.
@@ -28,22 +32,48 @@ public class Util {
         }
     }
 
-    public static int minimoVal(int[] array) {
-
-        int minimo = 10;
+    public static int numMinimo(int[] array) {
+        int valMinimo = 10;
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] <= minimo) {
-                minimo = array[i];
-
+            if (array[i] < valMinimo) {
+                valMinimo = array[i];
             }
         }
+        return valMinimo;
 
-        System.out.println("Il valore più piccolo è: " + minimo);
-        return minimo;
     }
 
+    public static int indiceMinimo(int[] array) {
+        int valMinimo = 10;
+        int indiceArray = 0;
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < valMinimo) {
+                indiceArray = i;
+            }
+        }
+        return indiceArray;
+
+    }
+
+    public static void sommaVettoriale(int[]array1, int[]array2) {
+        int[] sommaArray = new int [array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            sommaArray[i] = array1[i]+array2[i];
+
+        }
+        mostraSomma(sommaArray);
+    }
+
+    public static void mostraSomma(int[]array) {
+        System.out.println("La somma dei due array è:");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 
 
 
