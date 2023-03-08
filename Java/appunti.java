b1d98051f9a7507262f8c8d37ec3ecb5003b1ee6
Ecelon9 <Ecelon93@gmail.com>
2023-03-08 13:15:56 +0100

    lezione7 java

--- Java/appunti.java
+++ Java/appunti.java
@@ -7,16 +7,18 @@
  * Nella Stack, vengono impilati tutti i primitivi, come un numero, un carattere
  * mentre nella Heap vengono buttati a caso tutti gli oggetti complessi,
  * come una stringa, array, oggetti etc...
+ *  
+ * per convezione: le costanti (final) si scrivono 
+ * IN_MAIUSCOLO_CON_GLI_UNDERSCORE
  * 
+ * 	public final static String NOME_PERSONALE = "Matteo";
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ * Un metodo si dice ricorsivo quando nella firma c'è la chiamata a se stesso
+ * Attenzione che è come se fosse un while->true
+ * C'è bisogno di una condizione di base wrappata in una condizione di uscita
+ * E ogni volta che arriva la chiamata, si crea una nuova stack che va in pila
+ * fin quando non si arriva alla condizione d'uscita.
+ * A quel punto, la stack viene svuotata dall'alto, verso il basso.
  * 
  * 
  * 
