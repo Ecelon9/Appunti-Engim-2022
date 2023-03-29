9b8db2cb918485297e1a4cc5e1034e55bf386449
Ecelon9 <Ecelon93@gmail.com>
2023-03-29 20:03:28 +0200

    Aggiornamento batnav

--- BattagliaNavaleTemp/backend/source/classes/AssaultShip.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class AssaultShip extends Coesion {
-
-    public AssaultShip() {
-       super(2);
-    }
-
-}
--- BattagliaNavaleTemp/backend/source/classes/Cruiser.java
+++
@@ -1,9 +0,0 @@
-package classes;
-
-public class Cruiser extends Coesion{
-
-    public Cruiser() {
-        super(3);
-    }
-
-}
