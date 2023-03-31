debde5a6eca96314879368730e6337f5ea7fee24
Ecelon9 <Ecelon93@gmail.com>
2023-03-31 16:47:11 +0200

    progetto react

---
+++ react/src/components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header className="w-full h-10vh flex justify-end items-center text-4xl font-bold text-white bg-dark-green">
+            <div className="w-full flex justify-center">
+                <h1>ACCANNA</h1>
+            </div>
+            <div>
+                <span className="cursor-pointer">
+                    icon
+                </span>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
