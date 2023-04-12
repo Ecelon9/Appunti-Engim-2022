2e8307a0cd19dc7f02e83783bda7a9fd8ad12a5c
Ecelon9 <Ecelon93@gmail.com>
2023-04-12 13:24:30 +0200

    Nuovo progetto Hassan

--- Java/src/appunti.java
+++ Java/src/appunti.java
@@ -32,11 +32,41 @@
  * E ogni volta che arriva la chiamata, si crea una nuova stack che va in pila
  * fin quando non si arriva alla condizione d'uscita.
  * A quel punto, la stack viene svuotata dall'alto, verso il basso.
+ *
+ *
+ *
+ *                  Patten MVC (Module, View, Controll)
+ *
+ * è un'architettura, ovvero un modo in cui si strutturano le applicazioni
+ * a livello di directory/package
+ * Parlando di Dominio, si intente l'intera applicazione
+ *      1-Model -> entità dal db: Chi e come  viene rappresento all'interno del dominio
+ *      2-View  -> è la sezione di visualizzazione, ciò che si vede via web
+ *      3-Controller    -> si occupa dell'interazione fra View e Model interagendo col DB attraverso la repository
+ *      4-Repository    -> interfacce che definiscono i metodi CRUD (Create, Remove, Update, Delete)
+ *		Impl -> classi che chiamano il DB con metodi statici
+ *				
+ * Nella repository, per ogni attributo nel DB, si definiscono le interfacce
+ * con le quali si interagirà col DB
+ *      per esempio: getCategoryByName(String name);
+ * Queste quattro sono obbligatorie e i nomi sono importanti
+ * ma volendo se ne possono fare quante se ne vuole e i nomi sono liberi
+ *      5-Utils
+ *      6-Exceptions
+ *      7-Enums
+ *	8-Constant -> classi di variabili costanti
+ *
+ * Fino ad adesso abbiamo creato applicazioni MONOLITICHE
+ * ovvero, tutte le funzionalità risiedono all'interno dello stesso progetto
+ * e presenta il grosso problema che se la connessione al server va giù,
+ * l'intera applicazione smette di funzionare
+ *
+ * In realtà le applicazioni sono divise a MICROSERVIZI
+ * dove ogni singola funzionalità ha il proprio progetto staccato dagli altri
+ * e qualora dovesse andare giù un servizio, gli altri continuano a funzionare
  * 
- * 
- * 
- * 
- * 
+ * La prima cosa da fare, è creare la persistenza, ovvero la struttura del DB
+ *      (best practice, dopo averla usata, salvarsi la query su un file txt)
  * 
  * 
 */
