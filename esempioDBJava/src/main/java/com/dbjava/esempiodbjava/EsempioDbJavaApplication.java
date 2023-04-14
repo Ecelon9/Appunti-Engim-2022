326369b7b4770a22c7c9632454dd4219b143c1b4
Ecelon9 <Ecelon93@gmail.com>
2023-04-14 16:54:23 +0200

    Lezione react

---
+++ esempioDBJava/src/main/java/com/dbjava/esempiodbjava/EsempioDbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dbjava.esempiodbjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsempioDbJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsempioDbJavaApplication.class, args);
+    }
+
+}
---
+++ esempioDBJava/src/test/java/com/dbjava/esempiodbjava/EsempioDbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbjava.esempiodbjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsempioDbJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
